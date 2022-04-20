1549e172e1d89acceda7c6682019a05b2aff1e86
ohno418 <yutaro.ono.418@gmail.com>
2022-04-20 18:18:19 +0900

    Compile to return a number input

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
+use std::env;
+use std::process;
+
 fn main() {
-    let asm = compile();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Required only one argument.");
+        process::exit(1)
+    }
+
+    let asm = match compile(&args[1]) {
+        Ok(asm) => asm,
+        Err(err) => {
+            eprintln!("failed to compiler: {}", err);
+            process::exit(1);
+        }
+    };
     print!("{}", asm);
 }
 
-pub fn compile() -> String {
-    "\
+pub fn compile(input: &str) -> Result<String, String> {
+    let num: i32 = match input.parse() {
+        Ok(n) => n,
+        Err(_) => return Err("No".to_string()),
+    };
+
+    Ok(format!(
+        "\
     .intel_syntax noprefix
     .text
     .globl main
 main:
-    mov rax, 42
+    mov rax, {}
     ret
-".to_string()
+",
+        num
+    ))
 }
 
 #[cfg(test)]
@@ -19,7 +42,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn print_asm() {
+    fn returns_asm_to_return_input_number() {
         let expected = "\
     .intel_syntax noprefix
     .text
@@ -28,7 +51,12 @@ main:
     mov rax, 42
     ret
 ";
-        let actual = compile();
+        let actual = compile("42").unwrap();
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn returns_err_with_not_number_input() {
+        assert!(compile("hi").is_err());
     }
 }
