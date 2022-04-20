2de47bae283d47957165673a59a31b15a8f38b2d
ohno418 <yutaro.ono.418@gmail.com>
2022-04-20 17:45:07 +0900

    Generate assembly that returns 42

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
-    println!("Hello, world!");
+    let asm = compile();
+    print!("{}", asm);
+}
+
+pub fn compile() -> String {
+    "\
+    .intel_syntax noprefix
+    .text
+    .globl main
+main:
+    mov rax, 42
+    ret
+".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn print_asm() {
+        let expected = "\
+    .intel_syntax noprefix
+    .text
+    .globl main
+main:
+    mov rax, 42
+    ret
+";
+        let actual = compile();
+        assert_eq!(expected, actual);
+    }
 }
